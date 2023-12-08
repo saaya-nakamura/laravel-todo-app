b71e0654a8b453902013ed14b24a2ed3bd7200a2
saaya-nakamura <saaya.tsujino@gmail.com>
2023-12-08 17:04:49 +0900

    １回目のコミット

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+  <dic class="d-flex justify-content-center align-items-centerbg-light fixed-bottom" style="height: 60px;z-index:auto;">
+  <p class="text-muted small mb-0">&copy;{{config('app.name','Laravel')}} All rights reserved.</p>
+</div>
+</footer>
